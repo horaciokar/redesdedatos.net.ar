338fc8ce2e1875ce87af17e781aac3b6ac87132b
Horacio Karzon <horaciokar@gmail.com>
2023-06-28 03:04:26 +0000

    conec

--- js/conexion.js
+++ js/conexion.js
@@ -1,23 +1,21 @@
-const mysql = require('mysql');
-//require('dotenv').config(); // Para cargar las variables de entorno desde un archivo .env
+const mysql = require('mysql2');
 
-const dbConfig = {
-  host: "localhost",
-  user: "admin",
-  password: "1234567890",
-  database: "redesdedatos"
-};
-
-// Crear la conexión a la base de datos
-const connection = mysql.createConnection(dbConfig);
-
-// Conectar a la base de datos
-connection.connect((err) => {
-  if (err) {
-    console.error('Error al conectar a la base de datos:', err);
-    return;
-  }
-  console.log('Conexión exitosa a la base de datos');
+// Configurar la conexión a la base de datos
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'admin',
+  password: '1234567890',
+  database: 'redesdedatos'
 });
 
+// Realizar la conexión
+connection.connect((error) => {
+  if (error) {
+    console.error('Error al conectar a la base de datos:', error);
+  } else {
+    console.log('Conexión exitosa a la base de datos');
+  }
+});
+
+// Exportar la conexión
 module.exports = connection;
